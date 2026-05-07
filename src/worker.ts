8eb7125ec2782b927d5fdf40ae426b8d4de7bacf
kashif-23 <kashif.fg23@gmail.com>
2026-05-07 06:37:20 +0200

    udapted

--- src/worker.ts
+++ src/worker.ts
@@ -1,40 +1,42 @@
+/// <reference types="@cloudflare/workers-types" />
 import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler';
 
-interface Env {
-  __STATIC_CONTENT: KVNamespace;
+declare let __STATIC_CONTENT: KVNamespace;
+
+addEventListener('fetch', (event: FetchEvent) => {
+  event.respondWith(handleRequest(event));
+});
+
+async function handleRequest(event: FetchEvent): Promise<Response> {
+  const request = event.request;
+  if (request.method !== 'GET' && request.method !== 'HEAD') {
+    return new Response('Method Not Allowed', { status: 405 });
+  }
+
+  try {
+    return await getAssetFromKV(
+      {
+        request,
+        waitUntil(promise: Promise<any>) {
+          return event.waitUntil(promise);
+        },
+      } as any,
+      {
+        ASSET_NAMESPACE: __STATIC_CONTENT,
+        mapRequestToAsset,
+      },
+    );
+  } catch (err) {
+    return getAssetFromKV(
+      {
+        request: new Request(`${new URL(request.url).origin}/index.html`, request),
+        waitUntil(promise: Promise<any>) {
+          return event.waitUntil(promise);
+        },
+      } as any,
+      {
+        ASSET_NAMESPACE: __STATIC_CONTENT,
+      },
+    ).catch(() => new Response('Not Found', { status: 404 }));
+  }
 }
-
-export default {
-  async fetch(request: Request, env: Env, ctx: any): Promise<Response> {
-    if (request.method !== 'GET' && request.method !== 'HEAD') {
-      return new Response('Method Not Allowed', { status: 405 });
-    }
-
-    try {
-      return await getAssetFromKV(
-        {
-          request,
-          waitUntil(promise: Promise<any>) {
-            return ctx.waitUntil(promise);
-          },
-        } as any,
-        {
-          ASSET_NAMESPACE: env.__STATIC_CONTENT,
-          mapRequestToAsset,
-        },
-      );
-    } catch (error) {
-      return getAssetFromKV(
-        {
-          request: new Request(`${new URL(request.url).origin}/index.html`, request),
-          waitUntil(promise: Promise<any>) {
-            return ctx.waitUntil(promise);
-          },
-        } as any,
-        {
-          ASSET_NAMESPACE: env.__STATIC_CONTENT,
-        },
-      ).catch(() => new Response('Not Found', { status: 404 }));
-    }
-  },
-};
